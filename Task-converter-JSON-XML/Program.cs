53d2ece05f6cadd7e2058382527b714c6dbe8ff8
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2022-12-29 15:31:29 +0300

    Added initial solution

---
+++ Task-converter-JSON-XML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_converter_JSON_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
